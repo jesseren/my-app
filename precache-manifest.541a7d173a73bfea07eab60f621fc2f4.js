f9929963fda1c6af5fcc15037079b67be8283b64
jesseren <jesseren80@gmail.com>
2020-09-28 21:29:38 -0400

    Updates

---
+++ precache-manifest.541a7d173a73bfea07eab60f621fc2f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28d03c5f65c81faf308c8e82eac1403",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7e48ac599d2f5bbcd370",
+    "url": "/my-app/static/css/main.8a0c5f6f.chunk.css"
+  },
+  {
+    "revision": "43a30bbfa578caa6335c",
+    "url": "/my-app/static/js/2.71a1f868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.71a1f868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e48ac599d2f5bbcd370",
+    "url": "/my-app/static/js/main.3e00365c.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "474dc431dba8d50e849f40248887cc5d",
+    "url": "/my-app/static/media/Resume F2020.474dc431.pdf"
+  },
+  {
+    "revision": "e795ed0a26e5805aecbfc45bf7151d1b",
+    "url": "/my-app/static/media/me1.e795ed0a.jpg"
+  },
+  {
+    "revision": "625042432eb0b887b6b8b6759d762355",
+    "url": "/my-app/static/media/me2.62504243.JPG"
+  }
+]);
