ca2a9e06db70b848bfa5f39a90bda9f1abb9a766
jesseren <jesseren80@gmail.com>
2021-12-07 21:19:40 -0500

    Deploy Changes

---
+++ build/precache-manifest.eafd6110863df538212df89df3331ee8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2046555606a27129cfc6583ec7c77809",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d03effd4ffe5d2b389b9",
+    "url": "/my-app/static/css/main.8a0c5f6f.chunk.css"
+  },
+  {
+    "revision": "43a30bbfa578caa6335c",
+    "url": "/my-app/static/js/2.71a1f868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.71a1f868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03effd4ffe5d2b389b9",
+    "url": "/my-app/static/js/main.ff60c26a.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "10c131ab0a9a3f06b7cea03b96aa993d",
+    "url": "/my-app/static/media/Resume.10c131ab.pdf"
+  },
+  {
+    "revision": "e795ed0a26e5805aecbfc45bf7151d1b",
+    "url": "/my-app/static/media/me1.e795ed0a.jpg"
+  },
+  {
+    "revision": "625042432eb0b887b6b8b6759d762355",
+    "url": "/my-app/static/media/me2.62504243.JPG"
+  }
+]);
